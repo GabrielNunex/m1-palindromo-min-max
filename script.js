04d7b568f3de001c1887f6544dfbccaefd5acc9e
GabrielNunex <gabrielnunes.design@gmail.com>
2024-03-12 19:07:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    let text = str.toLowerCase();
+    let result = '';
+
+    //Loop para retirar espaços da frase
+    for (let i = 0; i < str.length; i++) {
+        if (text[i] != ' ') {
+            result += text[i];
+        }
+    }
+
+    //Loop para verificar letras nas extremidades
+    let peer = Math.floor(result.length/2)
+    let counter = 0
+    for (let i = 0; i < peer; i++) {
+        let compare = result.length - 1
+
+        if (result[i] == result[compare - i]) {
+            counter++;
+        } 
+    }
+
+    if (peer == counter) {
+        return true
+    } 
+        return false
 }
 
 function arrayMaxMin(arr){
